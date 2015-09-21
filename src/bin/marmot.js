6d7f302fbf5d151524bd8d1db3db8d524637fd56
evan2x <aiweizhang@creditease.cn>
2015-09-21 17:25:46 +0800

    change -r, --remove and -d, --delete params

--- src/bin/marmot.js
+++ src/bin/marmot.js
@@ -16,20 +16,21 @@ program
 // init sub command
 program
     .command('init')
-    .description('init project.')
-    .option('-f, --force', 'forced to redownload WEB-INF directory of the current project.')
+    .description('init project')
+    .option('-f, --force', 'forced to redownload WEB-INF directory of the current project')
     .action(init);
 
 // server sub command
 program
     .command('server')
-    .description('a tomcat server.')
-    .option('-p, --port [port]', 'specify the server listening [port] to start.', 8080)
-    .option('-s, --start', 'start the tomcat server.')
-    .option('-S, --stop', 'stop the tomcat server.')
-    .option('-c, --clean', 'cleanup the tomcat server.')
-    .option('-l, --list', 'list of tomcat services.')
-    .option('-r, --remove [name]', 'remove the service by [name].')
+    .description('a tomcat server')
+    .option('-p, --port [port]', 'specify the server listening [port] to start', 8080)
+    .option('-s, --start', 'start the tomcat server')
+    .option('-S, --stop', 'stop the tomcat server')
+    .option('-r, --restart', 'restart the tomcat server')
+    .option('-c, --clean', 'cleanup the tomcat server')
+    .option('-l, --list', 'list of tomcat services')
+    .option('-d, --delete [name]', 'delete the service by [name]')
     .action(server);
 
 program.parse(process.argv);
